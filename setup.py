029364cc305efca632b0d2c32b672bd428923739
Gabriel Mondragón <gabriel.mg@outlook.com>
2019-02-02 23:10:55 -0500

    Setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='nazk',
+    version='1.0.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/gmondragon/nazk',
+    license='GNU General Public License v3.0',
+    author='Gabriel Mondragón',
+    author_email='gabriel@mondragon.pe',
+    description='Get the official exchange rate of the SBS and SUNAT.',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
